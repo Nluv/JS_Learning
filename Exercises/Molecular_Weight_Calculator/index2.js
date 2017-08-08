dd84d691fc2e9ff86dccaef7c7efc686a1232140
RejuvenationXI <cornagliamattia@gmail.com>
2017-08-09 00:00:48 +0200

    Created a new object related exercises, work in progress

---
+++ Exercises/Molecular_Weight_Calculator/index2.js
@@ -0,0 +1,16 @@
+// function Element(number, name) {
+//   this.number = number;
+//   this.name = name;
+// }
+//
+// function arrayLoad(number, name) {
+//   var number = prompt("Inserisci un numero");
+//   var name = prompt("Inserisci un nome");
+//   const nuovo = new Element(number, name);
+//   return nuovo;
+// }
+//
+// console.log(arrayLoad());
+//
+// // const pippo = new Element("1", "Pippo");
+// // console.log(pippo);
