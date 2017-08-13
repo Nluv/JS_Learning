e575161c09f54ffd8d9a097f79a3cac5fac11c34
RejuvenationXI <cornagliamattia@gmail.com>
2017-08-13 12:23:53 +0200

    Added a few printed parameters

--- Exercises/Molecular_Weight_Calculator/index.js
+++ Exercises/Molecular_Weight_Calculator/index.js
@@ -137,11 +137,8 @@ function requestObject(number, name, symbol, atomic_weight, name_origin, density
 
 function printRequestedElement(id) {
   if (id > 0 && id < elements.length) {
-    // console.log(elements[id]);
     return elements[id];
   } else {
-    console.log(`${id} is not a valid value!! Please insert a number between 1 and 118!`);
+    return `${id} is not a valid value!! Please insert a number between 1 and 118!`;
   }
 }
-// var request = prompt("Quale elemento vuoi cercare? (Esprimi il numero sulla tavola periodica)");
-// printRequestedElement(request);
